80807e9ea23744cbae4a6c4dbf59f1ac0d07a1e6
harshanilgoyal <46393550+harshanilgoyal@users.noreply.github.com>
2019-03-15 18:56:23 +0530

    initial comit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+header:('login\login.php');
+
+?>
---
+++ libraries/links.php
@@ -0,0 +1,4 @@
+<link href=".\.\vendor\twbs\bootstrap\dist\css\bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src=".\.\vendor\twbs\bootstrap\dist\js\bootstrap.min.js"></script>
+  <script src=".\.\vendor\node_modules\jquery\dist\jquery.min.js"></script>
+  <link rel="stylesheet" href=".\.\vendor\fontawesome\css\all.css">
